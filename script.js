d61d94da0cdbfb5be65b6c644760b22fa0bcf346
Movie1337 <oldmooovie@gmail.com>
2023-08-07 14:55:22 +0300

    change 3

--- script.js
+++ script.js
@@ -1,75 +1,28 @@
+const apiKey = '0c1c169b53be4fbb82f81148230408'
+const mainMenu = document.getElementById("mainMenu");
 const search = document.getElementById("search"); 
 const randomScreen = document.getElementById("random");
-const mainMenu = document.getElementById("mainMenu");
 const screenLondon = document.getElementById("screenLondon");
+const cityScreen = document.getElementById("cityScreen");
 const buttonBack = document.getElementById("buttonBack");
 const buttonSearch = document.getElementById("buttonSearch");
-//const body = document.querySelector('#body');
+const form = document.querySelector('#form');
+const input = document.querySelector('#enter');
 
-function showScreen(id) {
-    document.querySelector(`#${id}`).classList.remove("hidden");
-}
-
-function hideScreen(id) {
-    document.querySelector(`#${id}`).classList.add("hidden");
-}
-
-const handleClickSearch = (event) => {
-    event.preventDefault();
-    hideScreen("mainMenu");
-    showScreen("screenLondon");
-}
-
-const handleClickBack = (event) => {
-    event.preventDefault();
-    hideScreen("screenLondon");
-    showScreen("mainMenu");
-}
-
-search.addEventListener("click", handleClickSearch);
-randomScreen.addEventListener("click", handleClickSearch);
-buttonBack.addEventListener("click", handleClickBack);
-buttonSearch.addEventListener("click", handleClickBack);
-
-//search.addEventListener("click", showScreen);
-/*
-function hideScreen(id) {
-    document.querySelector(`#${id}`).classList.remove("hidden");
-}
-
-buttonBack.addEventListener("click", hideScreen("mainMenu"));
-
-
-const showScreen = function () {
-    menu.classList.toggle("hidden");
-    screenLondon.classList.toggle("hidden"); 
-}
-
-search.addEventListener("click", showScreen);
-random.addEventListener("click", showScreen);
-
-const hideScreen = function () {
-    screenLondon.classList.toggle("hidden");
-    menu.classList.toggle("hidden");
-}
-
-buttonBack.addEventListener("click", hideScreen);
-buttonSearch.addEventListener("click", hideScreen);
-//показываем нужный город
-const showScreen = function (id) {
-    body.innerHTML = `<div id="london" class="london">
-    <div class="cityScreen">
-        <div class="buttons">
-            <button class="button button__back" name="button-back"></button>
-            <button class="button button__search" name="button-search"></button>
+//показываем карточку
+function showCard (temp, name, updated, icon) {
+    // разметка для карточки
+        cityScreen.innerHTML = `<div id="buttons" class="buttons">
+        <button id="buttonBack" class="button button__back" name="button-back"></button>
+        <button id="buttonSearch" class="button button__search" name="button-search"></button>
         </div>
         <div class="weather_card">
             <div class="weather_card__chilly">
-                <p class="weather_card__temperature">10</p>
-                <div class="weather_card__picture"><img src="icons/smallsun.png" alt=""></div>
+                <p class="weather_card__temperature">${temp}</p>
+                <div class="weather_card__picture">${icon}</div>
             </div>
-            <div class="weather_card__city_name">London</div>
-            <div class="weather_card__date">11. 06. 2023</div>
+            <div class="weather_card__city_name">${name}</div>
+            <div class="weather_card__date">${updated}</div>
             <div class="weather_card__week">
                 <div class="weather_card__day">
                     <div class="weather_card__calendar">
@@ -122,34 +75,72 @@ const showScreen = function (id) {
                     <span class="weather_card__number">13</span>
                 </div>
             </div>
-        </div>
-    </div>
-</div>`;
+        </div>`;
 }
-//нажатие кнопки search
-search.addEventListener('click', showScreen);
-//показываем главный экран
-const hideScreen = function (id) {
-    body.innerHTML = `<div id="mainMenu" class="menu">
-    <div class="mainScreen">
-        <div class="mainScreen__logo"><img src="icons/bigsun.png" alt=""></div>
-        <h1 class="mainScreen__title__one">Your forecast app</h1>
-        <div>
-            <div class="search">
-                <form class="form" id="form">
-                    <input id="enter" class="enter" type="text" placeholder="enter your city">
-                    <button id="search" type="button" class="search__icon" name="search-icon"><img src="icons/search.png" alt=""></button>
-                </form>
-            </div>
-        </div>
-        <div id="random" class="mainScreen__title__three"><a href="#">give me a random one</a></div>
-    </div>
-</div>`;
+
+//получаем данные с сервера
+async function getWeather(city) {
+    // адрес запроса
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
 }
-//нажатие кнопок back and search
-buttonBack.addEventListener('click', hideScreen);
-buttonSearch.addEventListener('click', hideScreen);
-//получаем название города 
-const form = document.querySelector('#form');
-const enter = document.querySelector('#enter');
-*/
+
+//скрывает основной экран 
+function hideScreen(id) {
+    const element = document.querySelector(`#${id}`);
+    element.classList.add("hidden");
+    setTimeout(() => {
+        element.classList.add("none");
+    }, 2000);
+}
+
+//показываем экран города
+function showScreen(id) {
+    const element = document.querySelector(`#${id}`);
+    setTimeout(() => {
+        element.classList.remove("none");
+    }, 2000);
+    setTimeout(() => {
+        element.classList.remove("hidden");
+    }, 2500);
+}
+
+//делаем отправку формы
+form.onsubmit = async function (e) {
+    // отменяем отправку формы
+    e.preventDefault();
+
+    // берём значение из инпута, обрезаем пробелы
+    let city = input.value.trim();
+    
+    // получаем данные с сервера
+    const data = await getWeather(city);
+
+    showCard(
+        data.current.temp_c,
+        data.location.name,
+        data.current.last_updated,
+        data.current.condition.icon,
+    );
+    
+    const handleClickSearch = (event) => {
+        event.preventDefault();
+        hideScreen("mainMenu");
+        showScreen("screenLondon");
+    }
+    
+    const handleClickBack = (event) => {
+        event.preventDefault();
+        hideScreen("screenLondon");
+        showScreen("mainMenu");
+    }
+    
+    search.addEventListener("click", handleClickSearch);
+    randomScreen.addEventListener("click", handleClickSearch);
+    buttonBack.addEventListener("click", handleClickBack);
+    buttonSearch.addEventListener("click", handleClickBack);
+    
+}
